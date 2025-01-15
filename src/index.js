354be9d6d05da11174caf2e4c94e7705a9d5f2b5
mdanieldantas <contatomarcosdgomes@gmail.com>
2025-01-15 14:36:39 -0300

    Adicionar configuração do servidor Express e criar páginas EJS para renderização

--- src/index.js
+++ src/index.js
@@ -1,32 +1,25 @@
-// Importa o módulo Express, que é um framework para criar servidores web e APIs no Node.js
-const express = require('express');
+const { isUtf8 } = require('buffer')
+const express = require('express')
+const path = require('path')
 
-// Importa o módulo Path, que fornece utilitários para trabalhar com caminhos de arquivos e diretórios
-const path = require('path');
+const app = express()
 
-// Cria uma instância do servidor Express
-const app = express();
+// Configuração do EJS como view engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
-// Configura o motor de template EJS para renderizar HTML dinâmico
-app.set('view engine', 'ejs');
+// Configuração para ler dados do formulário
+app.use(express.urlencoded({ extended: true }))
 
-// Define o diretório onde os templates EJS estão localizados
-app.set('views', path.join(__dirname, 'views'));
+// Configuração para servir arquivos estáticos
+app.use(express.static('public'))
 
-// Middleware para analisar o corpo das solicitações HTTP com dados codificados em URL
-app.use(express.urlencoded({ extended: true }));
+app.get("/",(req,res)=>{
+const title = "Projeto MVC"
+const mensage = "Bem vindo ao projeto MVC"
+    res.render('index',{title,mensage})
+})
 
-// Middleware para servir arquivos estáticos (como imagens, CSS, JavaScript) da pasta 'public'
-app.use(express.static('public'));
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Servidor iniciado!\nRodando em http://localhost:${PORT}/`))
 
-app.get("/", (req, res) => {
-    const title = "Home";
-    const mensage = "Bem vindo ao meu site";
-
-    res.render("index", { title, mensage });
-});
-
-// Define a porta na qual o servidor irá escutar. Usa a porta definida em process.env.PORT ou 3000 se não estiver definida
-const PORT = process.env.PORT || 3000;
-// Inicia o servidor e faz com que ele escute na porta definida. Exibe uma mensagem no console quando o servidor está pronto
-app.listen(PORT, () => console.log(`Servidor iniciado!\nRodando em http://localhost:${PORT}/`));
