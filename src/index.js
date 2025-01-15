bdd61f936ed707ab8369537a80899b0b3b673e84
mdanieldantas <contatomarcosdgomes@gmail.com>
2025-01-15 15:05:06 -0300

    Adicionar funcionalidade de registro de usuários e exibição de lista de usuários

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ const path = require('path')
 
 const app = express()
 
+const storedUsers = []
+
 // Configuração do EJS como view engine
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-// Configuração para ler dados do formulário
+
+
+// Configuração para ler dados do formulário html para que o express entenda e manipule os dados do formulário html para o servidor express ler as instrings do formulário
 app.use(express.urlencoded({ extended: true }))
 
 // Configuração para servir arquivos estáticos
@@ -22,8 +26,19 @@ const mensage = "Bem vindo ao projeto MVC"
 
 app.get("/formulario",(req,res)=>{
     res.render("form")
-    
 })
+
+app.post("/register",(req,res)=>{
+const username = req.body.username
+const password = req.body.password
+storedUsers.push({username,password})
+
+res.redirect("/usuarios")
+})
+
+app.get("/usuarios",(req,res)=>{
+    res.render("users",{users:storedUsers})
+});
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Servidor iniciado!\nRodando em http://localhost:${PORT}/`))
