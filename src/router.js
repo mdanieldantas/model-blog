b8633083b4ad92fae9f04700cfec7003428196b2
mdanieldantas <contatomarcosdgomes@gmail.com>
2025-01-16 08:40:50 -0300

    Adicionar roteador para gerenciar requisições de postagens

---
+++ src/router.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const postsController = require('./controllers/postsController')
+
+const router = express.Router()
+
+router.get('/', postsController.index)
+
+module.exports = router
