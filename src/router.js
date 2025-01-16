7fe63038dd73123ccec1bce2347eaacfcb78d90d
mdanieldantas <contatomarcosdgomes@gmail.com>
2025-01-16 09:24:46 -0300

    Adicionar rota para exibir postagens individuais

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ const postsController = require('./controllers/postsController')
 
 const router = express.Router()
 
-router.get('/', postsController.index)
+router.get('/', postsController.index);
+router.get("/posts/:id", postsController.show);
 
 module.exports = router
