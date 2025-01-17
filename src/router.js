fd9d4802d6c9c7fdab4bece774f6693fd45f9061
mdanieldantas <contatomarcosdgomes@gmail.com>
2025-01-17 13:50:47 -0300

    Adicionar rotas para o painel administrativo

--- src/router.js
+++ src/router.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const postsController = require('./controllers/postsController')
+const adminController = require('./controllers/adminController')
 
 const router = express.Router()
 
+//Rotas do Blog
 router.get('/', postsController.index);
 router.get("/posts/:id", postsController.show);
+
+//Rotas do Admin
+router.get("/admin",adminController.index);
+
+
 
 module.exports = router
